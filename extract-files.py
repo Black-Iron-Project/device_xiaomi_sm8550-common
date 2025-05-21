2112b89bd2c793d538bbf70dc0a47906abfa8c21
Lunark :3 <jvillaltamatos3007@gmail.com>
2025-05-21 12:01:25 -0500

    vermeer: Reuse input_suspend node for Lineage Health HAL
    This reverts commit 3fcb150ae6dbde391b30f8c4a893b1877da2f856.
    
    Change-Id: I90681e61b37616f928dce39a06155344dd88b5e0

--- extract-files.py
+++ extract-files.py
@@ -140,7 +140,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/sensors/hals.conf' : blob_fixup()
         .add_line_if_missing('sensors.xiaomi.v2.so'),
     'vendor/etc/ueventd.rc' : blob_fixup()
-        .add_line_if_missing('\n# Charger\n/sys/class/qcom-battery     night_charging            0660    system  system')
+        .add_line_if_missing('\n# Charger\n/sys/class/qcom-battery     input_suspend            0660    system  system')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
