620e996cf51f593b237f6fec8d11a01550b2eb46
ralf979 <ralf979@gmail.com>
2025-05-28 16:34:58 +0200

    vermeer: sign build

--- lineage_vermeer.mk
+++ lineage_vermeer.mk
@@ -29,3 +29,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=POCO/vermeer_global/vermeer:15/AQ3A.240912.001/OS2.0.102.0.VNKMIXM:user/release-keys
+
+ # Inherit from release keys
+ $(call inherit-product, vendor/lineage-priv/keys/keys.mk)
